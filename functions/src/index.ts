1c6c7ee172bc8c0392ff18bbc1a0e68e97fca552
Ben 'Bonesaw' Simmons <bdsimmons1@gmail.com>
2019-04-29 23:06:15 -0500

    WIP: Copy Assets cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,32 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const cors = require('cors')({ origin: true });
+
+const storage = admin.storage();
+const bucket = storage.bucket('mobile-ios-pocketnewz.appspot.com');
+
+export const cloneNewzAssets = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    const pathUrlPrefix = 'NewzReels';
+    const oldNewzId = request.query.oldNewzId;
+    const newNewzId = request.query.newNewzId;
+    const newAssetsPath = `${pathUrlPrefix}/${newNewzId}`
+
+    const options = {
+      prefix: `${pathUrlPrefix}/${oldNewzId}`
+    }
+
+    const [fileObjects] = await bucket.getFiles(options);
+    fileObjects.forEach(async file => {
+      const nameArr = file.name.split('/');
+      const name = nameArr[nameArr.length - 1];
+      await file.copy(`${newAssetsPath}/${name}`);
+      await file.
+    })
+
+    response.send(fileObjects);
+  })
+})
