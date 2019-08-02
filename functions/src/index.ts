551cdeb8db86fde3a0ff2c70042b8853e2e0b38b
Ben 'Bonesaw' Simmons <bdsimmons1@gmail.com>
2019-08-02 14:00:01 -0500

    Add setupNewUser function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,70 @@ const cors = require('cors')({ origin: true });
 const storage = admin.storage();
 const bucket = storage.bucket('mobile-ios-pocketnewz.appspot.com');
 
+const db = admin.firestore();
+
+export const setupNewUser = functions.auth.user().onCreate(async user => {
+  const userRef =        db.collection('UserInfo').doc(user.uid);
+  const stationsRef =    db.collection('Stations').doc(user.uid);
+  const newzerStatsRef = db.collection('NewzerStats').doc(user.uid);
+  try {
+    const userSnapshot = await userRef.get();
+    if (userSnapshot.exists) {
+      console.log(`The user ${user.uid} already exists. Do Nothing.`);
+    } else {
+      console.log(`The user ${user.uid} does not exist, let's create it in firestore`);
+      console.log(user);
+      let firstName = "";
+      let lastName = "";
+      if (user.displayName && user.displayName.length > 0) {
+        const nameArr = user.displayName.split(' ');
+        firstName = nameArr[0];
+        lastName = nameArr[1];
+      }
+      await userRef.set({
+        uid: user.uid,
+        email: user.email || "",
+        city: "",
+        country: "",
+        dob: "",
+        imageURL: user.photoURL ? user.photoURL : "",
+        firstName: firstName,
+        lastName: lastName,
+        phone: user.phoneNumber ? user.phoneNumber : "",
+        state: ""
+      });
+      
+      const newStationDoc = stationsRef.collection('MyStations').doc();
+      await newStationDoc.set({
+        coverPhotoURL: "",
+        createDate: admin.firestore.FieldValue.serverTimestamp(),
+        description: `PocketNewz Default Station`,
+        id: newStationDoc.id,
+        isCollaboration: false,
+        isPublic: true,
+        location: '',
+        ownerID: user.uid,
+        title: user.displayName ? `${user.displayName}'s Station` : `${user.email}'s Station`
+      })
+
+      const stationRefRef = db.collection('StationRef').doc(newStationDoc.id);
+      await stationRefRef.set({
+        isPublic: true,
+        key: `Stations/${user.uid}/MyStations/${newStationDoc.id}`,
+        newzCount: 0
+      })
+
+      await newzerStatsRef.set({
+        followers: 0,
+        newzCount: 0,
+        stations: 1
+      })
+    }
+  } catch(err) {
+    console.log(`There was an error requesting the user snapshot`);
+  }
+});
+
 export const cloneNewzAssets = functions.https.onRequest((request, response) => {
   cors(request, response, async () => {
     const pathUrlPrefix = 'NewzReels';
